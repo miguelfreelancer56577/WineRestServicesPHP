40517e811fd9fc1a1a2e892fced676974575c79d
miguelfreelancer56577 <miguelfreelancer56577@gmail.com>
2017-07-06 12:57:41 -0500

    Controller to manage all the catalogs in the application.

---
+++ application/controllers/Catalog_Services.php
@@ -0,0 +1,19 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+require APPPATH . '/libraries/Restservices.php';
+
+class Catalog_Services extends Restservices {
+
+    function __construct() {
+        parent::__construct();
+    }
+
+    public function position_post()
+    {
+    
+        
+
+    }
+    
+}
