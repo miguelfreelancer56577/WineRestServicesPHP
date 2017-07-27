2b7f91607e5b13f59b50f693bc3e356dc7950c62
miguelfreelancer56577 <miguelfreelancer56577@gmail.com>
2017-07-27 12:24:13 -0500

    I have created two beans, one for the employee table a other to save the information about of user

---
+++ application/beans/Employee.php
@@ -0,0 +1,15 @@
+<?php
+	
+	class Employee
+	{
+		public $id_employee;
+		public $first_name;
+		public $surname;
+		public $last_name;
+		public $age;
+		public $email;
+		public $id_position;
+		public $id_status;
+		
+	}
+?>
---
+++ application/beans/InfoUser.php
@@ -0,0 +1,8 @@
+<?php
+	
+	class InfoUser
+	{
+		public $user = null;
+		public $employee = null;
+	}
+?>
